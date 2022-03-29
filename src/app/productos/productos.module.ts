d663d17eebf1399f736a63170c0efea2fca1fc59
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-29 00:11:01 -0600

    Implementando un formulario reactivo en agregar.component

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
@@ -10,6 +11,7 @@ import { AgregarComponent } from './pages/agregar/agregar.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ProductosRoutingModule
   ]
 })
