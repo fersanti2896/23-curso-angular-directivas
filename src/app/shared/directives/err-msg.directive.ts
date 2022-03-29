f413b2ca0ce40500091cb59a4e2df9cd277aa237
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-29 01:13:17 -0600

    Cambiando el mensaje de la etiqueta y añadiendo clase en el err-msg.directive

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -5,8 +5,9 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 })
 export class ErrMsgDirective implements OnInit {
 
-  htmlElement   : ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
+  htmlElement     : ElementRef<HTMLElement>;
+  @Input() color  : string = 'red';
+  @Input() mensaje: string = 'Este campo es obligatorio';
 
   constructor( private el: ElementRef<HTMLElement> ) {
     console.log('constructor directive');
@@ -18,9 +19,15 @@ export class ErrMsgDirective implements OnInit {
   ngOnInit(): void {
     console.log('NgOnInit en la directiva');
     this.setColor();
+    this.setMensaje();
   }
 
   setColor(): void {
     this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 }
