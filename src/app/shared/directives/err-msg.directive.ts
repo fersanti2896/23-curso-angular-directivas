c5d75e763773084e2c6f3c65923e465d1d8dbc93
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-29 00:58:02 -0600

    Dando funcionalidad a err-msg.directive como cambiar color através de @Input

--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -1,15 +1,26 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrMsgDirective implements OnInit {
 
-  constructor() {
-    console.log('constructor directive');  
-  }
-  ngOnInit(): void {
-    console.log('NgOnInit en la directiva');
+  htmlElement   : ElementRef<HTMLElement>;
+  @Input() color: string = 'red';
+
+  constructor( private el: ElementRef<HTMLElement> ) {
+    console.log('constructor directive');
+    console.log(el); 
+    
+    this.htmlElement = el;
   }
 
+  ngOnInit(): void {
+    console.log('NgOnInit en la directiva');
+    this.setColor();
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this.color;
+  }
 }
